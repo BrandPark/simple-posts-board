94ae225e2dae8ba0aed2fa39718cee3745e6e775
brandpark <pak2005@hanmail.net>
2022-02-11 11:57:34 +0900

    Initial commit

---
+++ src/main/java/com/brandpark/simplepostsboard/SimplePostsBoardApplication.java
@@ -0,0 +1,13 @@
+package com.brandpark.simplepostsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePostsBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePostsBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandpark/simplepostsboard/SimplePostsBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandpark.simplepostsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePostsBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
